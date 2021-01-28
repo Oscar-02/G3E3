dc4599da37161ccaff5e975cf330297383178e58
Oscar-02 <70180715+Oscar-02@users.noreply.github.com>
2021-01-28 17:08:33 -0600

    Update Program.cs

--- G3E3/Program.cs
+++ G3E3/Program.cs
@@ -8,7 +8,7 @@ namespace G3E3
         {
             /*
              * PROGRAMACION ESTRUCTURADA GRUPO 07L
-             * EJERCICIO 4 GUIA 3
+             * EJERCICIO 3 GUIA 3
              * 
              * OSCAR ROLANDO CAÑAS VALDIZON
              */
